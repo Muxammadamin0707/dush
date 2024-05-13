c411fb88162f61159ee08124011a0d3f7d6e5364
Muxammadamin Zoirov <muxammadaminzoirov@gmail.com>
2024-05-13 11:41:38 +0500

    tugadi

---
+++ index.js
@@ -0,0 +1,11 @@
+let raqam = Number(prompt("haftakuni kiriting"))
+
+switch (raqam) {
+    case 1: alert("Dushanba"); break;
+    case 2: alert("Seshanba"); break;
+    case 3: alert("Juma");  break;
+    case 4: alert("Chorshanba"); break;
+    case 5: alert("Payshanbaa"); break;
+    case 6: alert("Shanba"); break;
+    case 7: alert("Yokashanba"); break;
+}
